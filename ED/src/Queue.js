b4b2e02fb55f6ee7579803169a49120ecc8d8835
Filipe Pires <liipepires@gmail.com>
2018-05-26 00:57:03 -0300

    estrutura de dados (fila)

---
+++ ED/src/Queue.js
@@ -0,0 +1,39 @@
+let Queue = ( () => {
+    const itens = new WeakMap();
+    class Queue {
+        constructor(){
+            itens.set(this, []);
+        }
+        get(){
+            return itens.get(this);
+        }
+        enqueue(elements){
+            if(elements.length > 1){
+                for(let element of elements){
+                    itens.get(this).push(element);
+                }
+            } else {
+                itens.get(this).push(elements);
+            }
+        }
+        dequeue(){
+            return itens.get(this).shift();
+        }
+        first(){
+            return itens.get(this)[0];
+        }
+        isEmpty(){
+            return (itens.get(this)).length === 0;
+        }
+        clear(){
+            itens.set(this, []);
+        }
+        size(){
+            return (itens.get(this)).length;
+        }
+    }
+    return Queue;
+})();
+
+exports.default = Queue;
+module.exports = exports['default'];
---
+++ ED/test/queue.js
@@ -0,0 +1,22 @@
+const Queue = require('./../src/Queue.js');
+
+let fila = new Queue;
+console.log(fila.isEmpty());
+fila.enqueue(1);
+console.log(fila.isEmpty());
+console.log(fila.get());
+
+fila.enqueue([2,4,5]);
+console.log(fila.get());
+console.log(fila.first());
+
+fila.clear();
+console.log(fila.get());
+
+fila.enqueue(10);
+console.log(fila.get());
+console.log(fila.dequeue());
+fila.enqueue(8);
+console.log(fila.get());
+fila.enqueue([9,11,12]);
+console.log(fila.get());
