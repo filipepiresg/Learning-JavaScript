8a9057e670a962aad07cf1cef25d12051e9e130d
Filipe Pires <liipepires@gmail.com>
2018-05-31 00:16:20 -0300

    funcao de hashCode djb2HashCode implementado

--- ED/src/Hash.js
+++ ED/src/Hash.js
@@ -5,6 +5,13 @@ const loseLoseHashCode = function(key=(Number || String)) {
     }
     return hash % 37;
 }
+const djb2HashCode = function(key=(Number || String)){
+    let hash = 5381;
+    for(let i=0; i<key.toString().length; i++){
+        hash = hash * 33 + key.toString().charCodeAt(i);
+    }
+    return hash % 1013;
+}
 
 const Hash = (() => {
     class Hash {
@@ -39,7 +46,8 @@ const Hash = (() => {
             this.colision = 0;
         }
         hashCode(key){
-            return loseLoseHashCode(key);
+            // return loseLoseHashCode(key);
+            return djb2HashCode(key);
         }
     };
     return Hash;
